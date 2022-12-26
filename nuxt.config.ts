257bbe042ae4abea8fd7b928e17b39cda8c33438
cxx <0001001bill@gmail.com>
2022-12-26 11:59:51 -0600

    try generate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 
 export default defineNuxtConfig({
-  target: 'static',
   modules: ['@nuxt/content', '@unocss/nuxt'],
+  // ssr: false,
+  // nitro: {
+  //   preset: 'service-worker'
+  // },
   experimental: {
     reactivityTransform: true,
     inlineSSRStyles: false,
