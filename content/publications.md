20e83f63714131619b4cf930ec68eb66072f1c0c
cxx <0001001bill@gmail.com>
2022-12-26 00:02:32 -0600

    update publication

--- content/publications.md
+++
@@ -1,5 +0,0 @@
----
-title: Publications
----
-
-1. blablabla
--- pages/publications.vue
+++ pages/publications.vue
@@ -1,23 +1,55 @@
+<script setup lang="ts">
+import type { QueryBuilderParams } from '@nuxt/content/dist/runtime/types'
+const { path } = useRoute()
+const query: QueryBuilderParams = { path: path, find: 'one' }
+
+</script>
+
 <template>
     <main>
-        <section class="hero-section">
-            <header>
-                <h1 class="font-black text-3xl">Publications</h1>
-            </header>
-        </section>
-        <section>
-            <article class="prose mx-auto">
-                <ContentDoc />
-            </article>
-        </section>
+        <article class="prose mx-auto">
+
+            <span>* Xiaoxing Cheng, Xiao-Xing Cheng, XX Cheng are all me.</span>
+            <ContentDoc :query=query v-slot="{ doc }">
+                <h3>Dissertation</h3>
+                <ul>
+                    <ol v-for="(dissertation, index) in doc.dissertation" v-bind:key="dissertation.id">
+                        <span class="w-8 inline-block">{{ index + 1 }}.</span>
+                        <span>{{ dissertation.author }}</span>.
+                        <span italic>{{ dissertation.title }}</span>.
+                        <span>{{ dissertation.school }}</span>,
+                        <span>{{ dissertation.year }}</span>
+                    </ol>
+                </ul>
+                <h3>Journal Articles</h3>
+                <ul>
+                    <ol v-for="(journal, index) in doc.journals" v-bind:key="journal.id">
+                        <span class="w-8 inline-block">{{ index + 1 }}.</span>
+                        <span>{{ journal.authors }}</span>,&nbsp;
+                        <span>"{{ journal.title }}</span>",&nbsp;
+                        <span inline-block italic font-semibold>{{ journal.journal }}</span>&nbsp;
+                        <span>({{ journal.year }})</span>.
+                    </ol>
+                </ul>
+                <h3>Books</h3>
+                <ul>
+                    <ol v-for="(book, index) in doc.books" v-bind:key="book.id">
+                        <span class="w-8 inline-block">{{ index + 1 }}.</span>
+                        <span>{{ book.title }}</span>,&nbsp;
+                        <span>({{ book.year }})</span>,&nbsp;
+                        <span>{{ book.authors }}</span>
+                    </ol>
+                </ul>
+            </ContentDoc>
+        </article>
     </main>
 </template>
   
 <style scoped>
-h1 {
+/* h1 {
     font-size: x-large;
     text-align: center;
     margin-top: 3rem;
     margin-bottom: 3rem;
-}
+} */
 </style>
